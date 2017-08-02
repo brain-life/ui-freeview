cf38b0b7ea347f1c2dff7b6a921e8bc4daeb342b
Soichi Hayashi <hayashis@iu.edu>
2017-08-02 22:13:59 +0000

    made xvnc the last thing to start in a hope that it prevents connection failurer

--- start.sh
+++ start.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-#for file in `find /input -name *.nii.gz`
-#do
-#    /usr/local/freesurfer/bin/freeview $file &
-#done
-
-
 #for single output (like from process view)
-if [ -d /input/output/output/mri ];
+if [ -d /input/output/mri ];
 then
     #for single freesurfer output
     /usr/local/freesurfer/bin/freeview -v \
-        /input/output/output/mri/T1.mgz \
-        /input/output/output/mri/wm.mgz \
-        /input/output/output/mri/brainmask.mgz \
-        /input/output/output/mri/aseg.mgz:colormap=lut:opacity=0.2 \
-        -f /input/output/output/surf/lh.white:edgecolor=blue \
-        /input/output/output/surf/lh.pial:edgecolor=red \
-        /input/output/output/surf/rh.white:edgecolor=blue \
-        /input/output/output/surf/rh.pial:edgecolor=red
+    /input/output/mri/T1.mgz \
+    /input/output/mri/wm.mgz \
+    /input/output/mri/brainmask.mgz \
+    /input/output/mri/aseg.mgz:colormap=lut:opacity=0.2 \
+    -f /input/output/surf/lh.white:edgecolor=blue \
+    /input/output/surf/lh.pial:edgecolor=red \
+    /input/output/surf/rh.white:edgecolor=blue \
+    /input/output/surf/rh.pial:edgecolor=red
 else
 
     #for multiple datasets downloaded
