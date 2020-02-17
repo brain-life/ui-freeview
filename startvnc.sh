4272dd1b8da652951ca973efb0a4fc957c53258e
Soichi Hayashi <hayashis@iu.edu>
2020-02-17 10:34:44 -0500

    re-enabled xfce4 window manager

--- startvnc.sh
+++ startvnc.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#echo "-----------------------------------Xvfb-------------------------------"
-#Xvfb :0 -screen 0 1200x800x16 +extension RANDR +extension GLX &
-##
-#echo "Waiting for Xvfb to be ready..."
-#while ! xdpyinfo -display :0; do
-#    echo -n ''
-#    sleep 0.1
-#done
-
-#echo "-----------------------------------lxde-----------------------------"
-#source $FREESURFER_HOME/SetUpFreeSurfer.sh
-
 echo "$X11VNC_PASSWORD" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 
